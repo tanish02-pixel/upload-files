17ba2cdcbf2710a7ce5426ef83aa89e245f39b98
tanish02-pixel <tanishsingh2115@gmail.com>
2025-06-19 02:50:08 +0530

    Update: improved MongoDB connection and cleaned code

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 import path from "path";
 import multer from "multer";
 import File from "./model/file.js";
 
-const app = express();
-const PORT = 8000;
 
-mongoose.connect("mongodb://127.0.0.1:27017/fileUploads").then(() => console.log("✅ MongoDB Connected"))
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("✅ MongoDB Connected"))
 
 
 const storage =multer.diskStorage({
