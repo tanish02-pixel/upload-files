6c03ae07600493fc0ba85552ef8166e24dff5aab
tanish02-pixel <tanishsingh2115@gmail.com>
2025-06-19 03:12:39 +0530

    Cleaned up mongoose connection options

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ const PORT = process.env.PORT || 8000;
 
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("✅ MongoDB Connected"))
+mongoose.connect(process.env.MONGO_URI)
 
 
 const storage =multer.diskStorage({
